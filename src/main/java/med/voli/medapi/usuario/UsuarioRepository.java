216a1b4cf1b8d4f9fb8d7eca4e375f039ef50871
Luciano Gomes <lucianogvda02@gmail.com>
2023-11-13 21:58:37 -0300

    feat: implementação inicial das configurações de autenticação

--- src/main/java/med/voli/medapi/usuario/UsuarioRepository.java
+++ src/main/java/med/voli/medapi/usuario/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package med.voli.medapi.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    UserDetails findByLogin(String login);
 }
