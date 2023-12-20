c44c9c1314200dcdf7507a3c00c0feec83e1ff2e
Luciano Gomes <lucianogvda02@gmail.com>
2023-12-19 21:13:13 -0300

    feat: implementa pré requisitos de agendamento de consultas

--- src/main/java/med/voli/medapi/consulta/domain/domain_specifications/PreRequisitosAgendamentoDeConsulta.java
+++ src/main/java/med/voli/medapi/consulta/domain/domain_specifications/PreRequisitosAgendamentoDeConsulta.java
@@ -1,7 +1,6 @@
 package med.voli.medapi.consulta.domain.domain_specifications;
 
 import med.voli.medapi.consulta.domain.domain_specifications.validation_interfaces.PreRequisitosAgendamentoDeConsultaInterface;
-import med.voli.medapi.consulta.domain.domain_specifications.validation_interfaces.ValidadorAgendamentoConsultaInterface;
 import med.voli.medapi.consulta.dto.AgendamentoConsulta;
 import med.voli.medapi.infra.exceptions.ValidacaoException;
 import org.springframework.beans.factory.annotation.Autowired;
