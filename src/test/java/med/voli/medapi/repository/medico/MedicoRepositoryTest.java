3b2df9bc8c819dd1ced25c5777d84c1c988812f5
Luciano Gomes <lucianogvda02@gmail.com>
2024-01-20 20:14:59 -0300

    test: setup inicial de testes

---
+++ src/test/java/med/voli/medapi/repository/medico/MedicoRepositoryTest.java
@@ -0,0 +1,18 @@
+package med.voli.medapi.repository.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void buscarMedicoDisponivelNaData() {
+    }
+}
